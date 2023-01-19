e0e03b8c54873ffca12f9231de3e3ddc7fdf0a74
MarcoAgosta <agostamarco@libero.it>
2023-01-19 16:26:10 +0100

    Creata pagina e stampati dei dati

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+
 Route::get('/', function () {
-    return view('welcome');
+
+    $esempio=["ciao", "sono", "un", "esempio"];
+
+    return view('home', [
+        "esempio" => $esempio
+    ]);
 });
